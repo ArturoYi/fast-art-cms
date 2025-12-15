b53afaf464392850c52797ee6d6975f534de77a0
陈艺任 <chenyiren@chenyirendeMac-mini.local>
2025-12-16 01:33:02 +0800

    保存提交

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,30 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import { NConfigProvider } from "naive-ui";
 </script>
 
 <template>
-  <div>
-    <a
-      href="https://vite.dev"
-      target="_blank">
-      <img
-        src="/vite.svg"
-        class="logo"
-        alt="Vite logo" />
-    </a>
-    <a
-      href="https://vuejs.org/"
-      target="_blank">
-      <img
-        src="./assets/vue.svg"
-        class="logo vue"
-        alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <n-config-provider>
+    <div>
+      <a
+        href="https://vite.dev"
+        target="_blank">
+        <img
+          src="/vite.svg"
+          class="logo"
+          alt="Vite logo" />
+      </a>
+      <a
+        href="https://vuejs.org/"
+        target="_blank">
+        <img
+          src="./assets/vue.svg"
+          class="logo vue"
+          alt="Vue logo" />
+      </a>
+    </div>
+    <HelloWorld msg="Vite + Vue" />
+  </n-config-provider>
 </template>
 
 <style scoped>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,55 @@
-import { defineConfig } from 'vite';
+/** @type {import('vite').UserConfig} */
+import { defineConfig, loadEnv, type ConfigEnv, type UserConfig } from 'vite';
+import { fileURLToPath, URL } from 'node:url';
 import vue from '@vitejs/plugin-vue';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
+  const { VITE_PORT, VITE_OPEN, VITE_VERSION } = loadEnv(mode, process.cwd());
+  return defineConfig({
+    define: {
+      __VERSION__: JSON.stringify(VITE_VERSION)
+    },
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      host: '0.0.0.0',
+      port: Number(VITE_PORT),
+      open: Boolean(VITE_OPEN)
+    },
+    build: {
+      rolldownOptions: {
+        output: {
+          advancedChunks: {
+            groups: [
+              {
+                name: 'vueuse',
+                test: /\/vueuse/
+              },
+              {
+                name: 'vue',
+                test: /\/vue/
+              },
+              {
+                name: 'naive-ui',
+                test: /\/naive-ui/
+              },
+              {
+                name: 'vue-router',
+                test: /\/vue-router/
+              },
+              {
+                name: 'pinia',
+                test: /\/pinia/
+              }
+            ]
+          }
+        }
+      }
+    }
+  });
 });
